3b962bcbdf15c32e567e67fd9d7ccba1a3c1f927
Zhao Enbao <russeldevsmart1998@gmail.com>
2021-03-07 17:39:04 +1000

    fix: build issues

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52a504f37f2bba0ba5e4a143a5019b35.js"
+  "/precache-manifest.1ce72a7aa5297250b76c3fa86a2775e9.js"
 );
 
 self.addEventListener('message', (event) => {
